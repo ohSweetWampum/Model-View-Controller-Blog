c51c24c5da6be630ba8be148570738d3600b7628
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-04 10:36:45 -0500

    defined comment class

--- models/Comment.js
+++ models/Comment.js
@@ -2,9 +2,9 @@ const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../config/connection.js");
 
-class Category extends Model {}
+class Comment extends Model {}
 
-Category.init(
+Comment.init(
   {
     // define columns( id:,category_name:)
     id: {
@@ -13,18 +13,38 @@ Category.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    category_name: {
+    comment_content: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    creation_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    blogpost_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "blogpost",
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "category",
+    modelName: "comment",
   }
 );
 
-module.exports = Category;
+module.exports = Comment;

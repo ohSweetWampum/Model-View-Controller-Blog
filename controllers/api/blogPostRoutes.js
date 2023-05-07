6cae6b4b6ae07b08b2620a893237432976307a57
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-07 16:52:10 -0500

    I believe I have completed the blogpost routes file and the commentroutes file. May go back later to tinker with it

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,85 +1,92 @@
 const router = require("express").Router();
-const { Category, Product } = require("../../models");
+const { BlogPost, User, Comment } = require("../../models");
+const authRequired = require("../../utils/auth");
 
-// The `/api/categories` endpoint
-// find all categories
-// be sure to include its associated Products
-
+// Get all blog posts
 router.get("/", async (req, res) => {
   try {
-    const categoryData = await Category.findAll({
-      include: [{ model: Product }],
+    const getAllBlogPosts = await BlogPost.findAll({
+      include: [{ model: User, attributes: ["username"] }],
     });
-    res.status(200).json(categoryData);
+    res.status(200).json(getAllBlogPosts);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// find one category by its `id` value
-// be sure to include its associated Products
+// Get a single blog post by its `id`
 router.get("/:id", async (req, res) => {
   try {
-    const categoryIdData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }],
+    const findBlogPost = await BlogPost.findByPk(req.params.id, {
+      include: [
+        { model: User, attributes: ["username"] },
+        {
+          model: Comment,
+          include: [{ model: User, attributes: ["username"] }],
+        },
+      ],
     });
 
-    if (!categoryIdData) {
-      res.status(404).json({ message: "No category found with that id!" });
+    if (!findBlogPost) {
+      res.status(404).json({ message: "We couldn't find a blog post with that id" });
       return;
     }
 
-    res.status(200).json(categoryIdData);
+    res.status(200).json(findBlogPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// create a new category
-router.post("/", async (req, res) => {
+// Create a new blog post
+router.post("/", authRequired, async (req, res) => {
   try {
-    const newCategoryData = await Category.create(req.body);
-
-    res.status(200).json(newCategoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-// update a category by its `id` value
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedCategoryData = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
+    const newBlogPost = await BlogPost.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
 
-    if (updatedCategoryData) {
-      res.status(200).json(updatedCategoryData);
-    } else {
-      res.status(404).json({ message: "Category not found" });
-    }
+    res.status(200).json(newBlogPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// delete a category by its `id` value
-
-router.delete("/:id", async (req, res) => {
+// Update a blog post by its `id`
+router.put("/:id", authRequired, async (req, res) => {
   try {
-    const categoryData = await Category.destroy({
+    const updateBlogPost = await BlogPost.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
 
-    if (!categoryData) {
-      res.status(404).json({ message: "No Category found with that id!" });
+    if (!uupdateBlogPost[0]) {
+      res.status(404).json({ message: "We couldn't find a blog post with that id" });
       return;
     }
 
-    res.status(200).json(categoryData);
+    res.status(200).json(updateBlogPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete a blog post by its `id`
+router.delete("/:id", authRequired, async (req, res) => {
+  try {
+    const deleteBlogPost = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteBlogPost) {
+      res.status(404).json({ message: "We couldn't find a blog post with that id" });
+      return;
+    }
+
+    res.status(200).json(deleteBlogPost);
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,130 +1,51 @@
 const router = require("express").Router();
-const { Product, Category, Tag, ProductTag } = require("../../models");
+const { Comment, User, BlogPost } = require("../../models");
+const authRequired = require("../../utils/auth");
 
-// The `/api/products` endpoint
-
-// get all products
-// find all products
-// be sure to include its associated Category and Tag data
+// Get all comments
 router.get("/", async (req, res) => {
   try {
-    const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    const getAllComments = await Comment.findAll({
+      include: [{ model: User, attributes: ["username"] }],
     });
-    res.status(200).json(productData);
+    res.status(200).json(getAllComments);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// find a single product by its `id`
-// be sure to include its associated Category and Tag data
-// get one product
-router.get("/:id", async (req, res) => {
+// Create a new comment
+router.post("/", authRequired, async (req, res) => {
   try {
-    const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    const createComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
 
-    if (!productData) {
-      res.status(404).json({ message: "No product found with that id!" });
-      return;
-    }
-
-    res.status(200).json(productData);
+    res.status(200).json(createComment);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
-// create new product
-router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
-
-// update product
-router.put("/:id", (req, res) => {
-  // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
-});
-// delete one product by its `id` value
-router.delete("/:id", async (req, res) => {
+// Delete a comment by its `id` value
+router.delete("/:id", authRequired, async (req, res) => {
   try {
-    const productData = await Product.destroy({
+    const deleteComment = await Comment.destroy({
       where: {
         id: req.params.id,
+        user_id: req.session.user_id,
       },
     });
 
-    if (!productData) {
-      res.status(404).json({ message: "No product found with that id!" });
+    if (!deleteComment) {
+      res
+        .status(404)
+        .json({ message: "We couldn't find a comment with that id" });
       return;
     }
 
-    res.status(200).json(productData);
+    res.status(200).json(deleteComment);
   } catch (err) {
     res.status(500).json(err);
   }

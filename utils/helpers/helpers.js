77a9201d8bb0e9ea984e92a91883b98f05b7f33b
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-07 20:49:05 -0500

    completed rough drafts for all of my .handlebars files. Also finished my server.js file

--- utils/helpers/helpers.js
+++ utils/helpers/helpers.js
@@ -1,12 +1,10 @@
-// still need to make changes to this but mostly done
-
 module.exports = {
-  format_time: (date) => {
+  formatTime: (date) => {
     return date.toLocaleTimeString();
   },
-  format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
-    }`;
+  formatDate: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
   },
 };

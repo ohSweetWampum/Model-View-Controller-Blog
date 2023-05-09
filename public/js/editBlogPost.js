52f69bf299625211067eb5c623f5c4b30ef5323f
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-09 11:15:08 -0500

    fixed mismatched names

--- public/js/editBlogPost.js
+++ public/js/editBlogPost.js
@@ -2,15 +2,18 @@ async function editBlogPost(event) {
   event.preventDefault();
 
   const id = window.location.pathname.split("/")[2];
-  const title = document.querySelector("#blogpost_title").value.trim();
-  const content = document.querySelector("#blogpost_content").value.trim();
+  const blogpost_title = document.querySelector("#blogpost_title").value.trim();
+  const blogpost_content = document
+    .querySelector("#blogpost_content")
+    .value.trim();
 
-  if (id && title && content) {
+  if (id && blogpost_title && blogpost_content) {
     const response = await fetch(`/api/blogposts/${id}`, {
       method: "PUT",
-      body: JSON.stringify({ title, content }),
+      body: JSON.stringify({ blogpost_title, blogpost_content }),
       headers: { "Content-Type": "application/json" },
     });
+    console.log(response);
 
     if (response.ok) {
       document.location.replace("/dashboard");
